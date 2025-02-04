7c1dcbc56d9bbb2de7278b4c33d0f2a000cda44e
Yuval Kohavi <yuval.kohavi@gmail.com>
2025-02-04 11:06:18 -0500

    rought draft of req + resp transform

--- internal/kgateway/extensions2/plugins/routepolicy/route_policy_plugin.go
+++ internal/kgateway/extensions2/plugins/routepolicy/route_policy_plugin.go
@@ -5,6 +5,8 @@ import (
 	"time"
 
 	envoyhttp "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/http_connection_manager/v3"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/anypb"
 	"google.golang.org/protobuf/types/known/durationpb"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 
@@ -18,12 +20,19 @@ import (
 	extensionsplug "github.com/kgateway-dev/kgateway/v2/internal/kgateway/extensions2/plugin"
 	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/ir"
 	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/plugins"
+	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/utils"
 	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/utils/krtutil"
 )
 
 type routePolicy struct {
 	ct   time.Time
-	spec v1alpha1.RoutePolicySpec
+	spec routeSpecIr
+}
+
+type routeSpecIr struct {
+	timeout   *durationpb.Duration
+	transform *anypb.Any
+	errors    []error
 }
 
 func (d *routePolicy) CreationTime() time.Time {
@@ -35,7 +44,15 @@ func (d *routePolicy) Equals(in any) bool {
 	if !ok {
 		return false
 	}
-	return d.spec == d2.spec
+
+	if !proto.Equal(d.spec.timeout, d2.spec.timeout) {
+		return false
+	}
+	if !proto.Equal(d.spec.transform, d2.spec.transform) {
+		return false
+	}
+
+	return true
 }
 
 type routePolicyPluginGwPass struct {
@@ -80,6 +97,115 @@ func NewPlugin(ctx context.Context, commoncol *common.CommonCollections) extensi
 	}
 }
 
+func toSpec(spec v1alpha1.RoutePolicySpec) routeSpecIr {
+	var ret routeSpecIr
+	if spec.Timeout > 0 {
+		ret.timeout = durationpb.New(time.Second * time.Duration(spec.Timeout))
+	}
+	var err error
+	ret.transform, err = toTransformFilterConfig(&spec.Transformation)
+	if err != nil {
+		ret.errors = append(ret.errors, err)
+	}
+
+	return ret
+}
+
+func toTransform(t *v1alpha1.Transform) *transformationpb.Transformation_TransformationTemplate {
+
+	hasTransform := false
+	tt := &transformationpb.Transformation_TransformationTemplate{
+		TransformationTemplate: &transformationpb.TransformationTemplate{
+			Headers: map[string]*transformationpb.InjaTemplate{},
+		},
+	}
+	for _, h := range t.Set {
+		tt.TransformationTemplate.Headers[string(h.Name)] = &transformationpb.InjaTemplate{
+			Text: string(h.Value),
+		}
+		hasTransform = true
+	}
+
+	for _, h := range t.Add {
+		tt.TransformationTemplate.HeadersToAppend = append(tt.TransformationTemplate.HeadersToAppend, &transformationpb.TransformationTemplate_HeaderToAppend{
+			Key: string(h.Name),
+			Value: &transformationpb.InjaTemplate{
+				Text: string(h.Value),
+			},
+		})
+		hasTransform = true
+	}
+
+	tt.TransformationTemplate.HeadersToRemove = make([]string, 0, len(t.Remove))
+	for _, h := range t.Remove {
+		tt.TransformationTemplate.HeadersToRemove = append(tt.TransformationTemplate.HeadersToRemove, string(h))
+		hasTransform = true
+	}
+
+	//BODY
+	if t.Body == nil {
+		tt.TransformationTemplate.BodyTransformation = &transformationpb.TransformationTemplate_Passthrough{
+			Passthrough: &transformationpb.Passthrough{},
+		}
+	} else {
+		if t.Body.ParseAs == v1alpha1.BodyParseBehaviorAsString {
+			tt.TransformationTemplate.ParseBodyBehavior = transformationpb.TransformationTemplate_DontParse
+		}
+		if value := t.Body.Value; value != nil {
+			hasTransform = true
+			tt.TransformationTemplate.BodyTransformation = &transformationpb.TransformationTemplate_Body{
+				Body: &transformationpb.InjaTemplate{
+					Text: string(*value),
+				},
+			}
+		}
+	}
+
+	if !hasTransform {
+		return nil
+	}
+	return tt
+}
+
+func toTransformFilterConfig(t *v1alpha1.TransformationPolicy) (*anypb.Any, error) {
+	if t == nil {
+		return nil, nil
+	}
+
+	var reqt *transformationpb.Transformation
+	var respt *transformationpb.Transformation
+
+	if rtt := toTransform(t.Request); rtt != nil {
+		reqt = &transformationpb.Transformation{
+			TransformationType: rtt,
+		}
+	}
+	if rtt := toTransform(t.Response); rtt != nil {
+		respt = &transformationpb.Transformation{
+			TransformationType: rtt,
+		}
+	}
+	if reqt == nil && respt == nil {
+		return nil, nil
+	}
+	reqm := &transformationpb.RouteTransformations_RouteTransformation_RequestMatch{
+		RequestTransformation:  reqt,
+		ResponseTransformation: respt,
+	}
+
+	envoyT := &transformationpb.RouteTransformations{
+		Transformations: []*transformationpb.RouteTransformations_RouteTransformation{
+			{
+				Match: &transformationpb.RouteTransformations_RouteTransformation_RequestMatch_{
+					RequestMatch: reqm,
+				},
+			},
+		},
+	}
+
+	return utils.MessageToAny(envoyT)
+}
+
 func convert(targetRef v1alpha1.LocalPolicyTargetReference) []ir.PolicyTargetRef {
 	return []ir.PolicyTargetRef{{
 		Kind:  string(targetRef.Kind),
@@ -109,8 +235,16 @@ func (p *routePolicyPluginGwPass) ApplyForRoute(ctx context.Context, pCtx *ir.Ro
 		return nil
 	}
 
-	if policy.spec.Timeout > 0 && outputRoute.GetRoute() != nil {
-		outputRoute.GetRoute().Timeout = durationpb.New(time.Second * time.Duration(policy.spec.Timeout))
+	if policy.spec.timeout != nil && outputRoute.GetRoute() != nil {
+		outputRoute.GetRoute().Timeout = policy.spec.timeout
+	}
+
+	if policy.spec.transform != nil {
+		if outputRoute.TypedPerFilterConfig == nil {
+			outputRoute.TypedPerFilterConfig = make(map[string]*anypb.Any)
+		}
+		outputRoute.TypedPerFilterConfig[filterStage] = policy.spec.transform
+		p.needFilter = true
 	}
 
 	return nil
