16f3a57f38007919aa6f74c77dda1edfc9598941
nfuden <nathan.fudenberg@solo.io>
2025-03-04 11:09:03 -0500

    test/e2e: transformation kube e2e tests

---
+++ test/kubernetes/e2e/features/transformation/suite.go
@@ -0,0 +1,120 @@
+package transformation
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/stretchr/testify/suite"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/kgateway-dev/kgateway/v2/pkg/utils/kubeutils"
+	"github.com/kgateway-dev/kgateway/v2/pkg/utils/requestutils/curl"
+	testmatchers "github.com/kgateway-dev/kgateway/v2/test/gomega/matchers"
+	"github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e"
+	testdefaults "github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e/defaults"
+)
+
+var _ e2e.NewSuiteFunc = NewTestingSuite
+
+// testingSuite is a suite of basic routing / "happy path" tests
+type testingSuite struct {
+	suite.Suite
+
+	ctx context.Context
+
+	// testInstallation contains all the metadata/utilities necessary to execute a series of tests
+	// against an installation of kgateway
+	testInstallation *e2e.TestInstallation
+}
+
+func NewTestingSuite(ctx context.Context, testInst *e2e.TestInstallation) suite.TestingSuite {
+	return &testingSuite{
+		ctx:              ctx,
+		testInstallation: testInst,
+	}
+}
+
+func (s *testingSuite) TestGatewayWithTransformedRoute() {
+	manifests := []string{
+		testdefaults.CurlPodManifest,
+		simpleServiceManifest,
+		gatewayWithRouteManifest,
+	}
+	manifestObjects := []client.Object{
+		testdefaults.CurlPod, // curl
+		simpleSvc,            // echo service
+		// nginxPod, exampleSvc, // nginx
+		proxyService, proxyServiceAccount, proxyDeployment, // proxy
+	}
+
+	s.T().Cleanup(func() {
+		for _, manifest := range manifests {
+			err := s.testInstallation.Actions.Kubectl().DeleteFileSafe(s.ctx, manifest)
+			s.Require().NoError(err)
+		}
+		s.testInstallation.Assertions.EventuallyObjectsNotExist(s.ctx, manifestObjects...)
+	})
+	// return
+
+	for _, manifest := range manifests {
+		err := s.testInstallation.Actions.Kubectl().ApplyFile(s.ctx, manifest)
+		s.Require().NoError(err)
+	}
+	s.testInstallation.Assertions.EventuallyObjectsExist(s.ctx, manifestObjects...)
+
+	// make sure pods are running
+	s.testInstallation.Assertions.EventuallyPodsRunning(s.ctx, testdefaults.CurlPod.GetNamespace(), metav1.ListOptions{
+		LabelSelector: "app.kubernetes.io/name=curl",
+	})
+
+	s.testInstallation.Assertions.EventuallyPodsRunning(s.ctx, proxyObjectMeta.GetNamespace(), metav1.ListOptions{
+		LabelSelector: "app.kubernetes.io/name=gw",
+	})
+
+	// time.Sleep(time.Hour)
+
+	testCasess := []struct {
+		name string
+		opts []curl.Option
+		resp *testmatchers.HttpResponse
+	}{
+		{
+			name: "basic",
+			opts: []curl.Option{
+				curl.WithBody("hello"),
+			},
+			resp: &testmatchers.HttpResponse{
+				StatusCode: http.StatusOK,
+				Headers: map[string]interface{}{
+					"x-foo-response": "notsuper",
+				},
+			},
+		},
+		{
+			name: "conditional set by request header", // inja and the request_header function in use
+			opts: []curl.Option{
+				curl.WithBody("hello"),
+				curl.WithHeader("x-add-bar", "super"),
+			},
+			resp: &testmatchers.HttpResponse{
+				StatusCode: http.StatusOK,
+				Headers: map[string]interface{}{
+					"x-foo-response": "supersupersuper",
+				},
+			},
+		},
+	}
+	for _, tc := range testCasess {
+		s.testInstallation.Assertions.AssertEventualCurlResponse(
+			s.ctx,
+			testdefaults.CurlPodExecOpt,
+			append(tc.opts,
+				curl.WithHost(kubeutils.ServiceFQDN(proxyObjectMeta)),
+				curl.WithHostHeader("example.com"),
+				curl.WithPort(8080),
+			),
+			tc.resp)
+	}
+
+}
