46fd6a35dc2e435eee36b12d5ebc7bb978237862
nfuden <nathan.fudenberg@solo.io>
2025-03-04 14:04:42 -0500

    test/transformation: simple rustformations

--- test/kubernetes/e2e/features/transformation/suite.go
+++ test/kubernetes/e2e/features/transformation/suite.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/stretchr/testify/suite"
+	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -42,9 +43,8 @@ func (s *testingSuite) TestGatewayWithTransformedRoute() {
 		gatewayWithRouteManifest,
 	}
 	manifestObjects := []client.Object{
-		testdefaults.CurlPod, // curl
-		simpleSvc,            // echo service
-		// nginxPod, exampleSvc, // nginx
+		testdefaults.CurlPod,                               // curl
+		simpleSvc,                                          // echo service
 		proxyService, proxyServiceAccount, proxyDeployment, // proxy
 	}
 
@@ -55,7 +55,6 @@ func (s *testingSuite) TestGatewayWithTransformedRoute() {
 		}
 		s.testInstallation.Assertions.EventuallyObjectsNotExist(s.ctx, manifestObjects...)
 	})
-	// return
 
 	for _, manifest := range manifests {
 		err := s.testInstallation.Actions.Kubectl().ApplyFile(s.ctx, manifest)
@@ -72,7 +71,104 @@ func (s *testingSuite) TestGatewayWithTransformedRoute() {
 		LabelSelector: "app.kubernetes.io/name=gw",
 	})
 
-	// time.Sleep(time.Hour)
+	testCasess := []struct {
+		name string
+		opts []curl.Option
+		resp *testmatchers.HttpResponse
+	}{
+		{
+			name: "basic",
+			opts: []curl.Option{
+				curl.WithBody("hello"),
+			},
+			resp: &testmatchers.HttpResponse{
+				StatusCode: http.StatusOK,
+				Headers: map[string]interface{}{
+					"x-foo-response": "notsuper",
+				},
+			},
+		},
+		{
+			name: "conditional set by request header", // inja and the request_header function in use
+			opts: []curl.Option{
+				curl.WithBody("hello"),
+				curl.WithHeader("x-add-bar", "super"),
+			},
+			resp: &testmatchers.HttpResponse{
+				StatusCode: http.StatusOK,
+				Headers: map[string]interface{}{
+					"x-foo-response": "supersupersuper",
+				},
+			},
+		},
+	}
+	for _, tc := range testCasess {
+		s.testInstallation.Assertions.AssertEventualCurlResponse(
+			s.ctx,
+			testdefaults.CurlPodExecOpt,
+			append(tc.opts,
+				curl.WithHost(kubeutils.ServiceFQDN(proxyObjectMeta)),
+				curl.WithHostHeader("example.com"),
+				curl.WithPort(8080),
+			),
+			tc.resp)
+	}
+}
+
+func (s *testingSuite) TestGatewayRustformationsWithTransformedRoute() {
+
+	manifests := []string{
+		testdefaults.CurlPodManifest,
+		simpleServiceManifest,
+		gatewayWithRouteManifest,
+	}
+	manifestObjects := []client.Object{
+		testdefaults.CurlPod,                               // curl
+		simpleSvc,                                          // echo service
+		proxyService, proxyServiceAccount, proxyDeployment, // proxy
+	}
+
+	controllerDeploymentOriginal := &appsv1.Deployment{}
+	err := s.testInstallation.ClusterContext.Client.Get(s.ctx, client.ObjectKey{
+		Namespace: s.testInstallation.Metadata.InstallNamespace,
+		Name:      "kgateway",
+	}, controllerDeploymentOriginal)
+	s.Assert().NoError(err, "has controller deploymnet")
+
+	controllerDeploy := controllerDeploymentOriginal.DeepCopy()
+	// add the environment variable RUSTFORMATIONS to the controller deployment
+	controllerDeploy.Spec.Template.Spec.Containers[0].Env = append(controllerDeploy.Spec.Template.Spec.Containers[0].Env, appsv1.EnvVar{
+		Name:  "RUSTFORMATIONS",
+		Value: "true",
+	})
+	// patch the actual deployment with the new environment variable
+	err = s.testInstallation.ClusterContext.Client.Patch(s.ctx, controllerDeploy, client.MergeFrom(controllerDeploy))
+	s.Assert().NoError(err, "patching controller deployment")
+
+	s.T().Cleanup(func() {
+		for _, manifest := range manifests {
+			err := s.testInstallation.Actions.Kubectl().DeleteFileSafe(s.ctx, manifest)
+			s.Require().NoError(err)
+		}
+		s.testInstallation.Assertions.EventuallyObjectsNotExist(s.ctx, manifestObjects...)
+		err = s.testInstallation.ClusterContext.Client.Patch(s.ctx, controllerDeploy, client.MergeFrom(controllerDeploy))
+		s.Require().NoError(err)
+	})
+
+	for _, manifest := range manifests {
+		err := s.testInstallation.Actions.Kubectl().ApplyFile(s.ctx, manifest)
+		s.Require().NoError(err)
+	}
+	s.testInstallation.Assertions.EventuallyObjectsExist(s.ctx, manifestObjects...)
+
+	// make sure pods are running
+	s.testInstallation.Assertions.EventuallyPodsRunning(s.ctx, testdefaults.CurlPod.GetNamespace(), metav1.ListOptions{
+		LabelSelector: "app.kubernetes.io/name=curl",
+	})
+
+	s.testInstallation.Assertions.EventuallyPodsRunning(s.ctx, proxyObjectMeta.GetNamespace(), metav1.ListOptions{
+		LabelSelector: "app.kubernetes.io/name=gw",
+	})
 
 	testCasess := []struct {
 		name string
