bd551ff3afd12ac1317ad6e69a5fb623b1739557
Lawrence Gadban <lawrence.gadban@solo.io>
2025-03-27 19:57:22 -0500

    make extauth provider use grpcService type
    Signed-off-by: nfuden <nathan.fudenberg@solo.io>

--- api/v1alpha1/gateway_extensions_types.go
+++ api/v1alpha1/gateway_extensions_types.go
@@ -36,19 +36,19 @@ const (
 
 // ExtAuthProvider defines the configuration for an ExtAuth provider.
 type ExtAuthProvider struct {
-	// BackendRef references the backend service that will handle the authentication.
+	// GrpcService is the GRPC service that will handle the processing.
 	// +kubebuilder:validation:Required
-	BackendRef *gwv1.BackendRef `json:"backendRef"`
+	GrpcService *ExtGrpcService `json:"grpcService"`
 }
 
 // ExtProcProvider defines the configuration for an ExtProc provider.
 type ExtProcProvider struct {
 	// GrpcService is the GRPC service that will handle the processing.
 	// +kubebuilder:validation:Required
-	GrpcService *ExtProcGrpcService `json:"grpcService"`
+	GrpcService *ExtGrpcService `json:"grpcService"`
 }
 
-type ExtProcGrpcService struct {
+type ExtGrpcService struct {
 	// BackendRef references the backend GRPC service that will handle the processing.
 	// +kubebuilder:validation:Required
 	BackendRef *gwv1.BackendRef `json:"backendRef"`
